6c67f7a9c8bf26335a854abf5e17782c23bc7c51
Code Ape <fardin.barashi@gmail.com>
2023-05-11 08:54:28 +0200

    Update Ps 5.x Template 2023-01-31.ps1

--- Template Powershell version 5.x/Ps 5.x Template 2023-01-31.ps1
+++ Template Powershell version 5.x/Ps 5.x Template 2023-01-31.ps1
@@ -9,12 +9,12 @@ WSManStackVersion              3.0
 PSRemotingProtocolVersion      2.3                                                                                                                  
 SerializationVersion           1.1.0.1      
 
-About Script 
-Author :
-Title : 
-Description : 
-Version : 
-Release day : 
+About Script : Template for Ps-5 Scripts
+Author : Fardin Barashi
+Title : Template Powershell version 5.x
+Description : A quick startup-template
+Version : 1.0
+Release day : 2023-01-31
 Github Link  : https://github.com/fardinbarashi
 News : 
 
